de51d06a6319f3e44dea6e135d8d7215cbb49878
Muhammad Khayat <khayat@infest.or.id>
2018-10-11 01:42:47 +0700

    retouch homepage and delete package-lock.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,17 @@
 import { OnInit, Component } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Gerbang Sulteng';
+  apiUrl = environment.api.graphql;
   constructor(private authService: AuthService) {}
   ngOnInit() {
-    /*if(this.authService.isLoggedIn){
-      this.user = this.authService.getUserDetail();
-      if(this.user.profile.sub.includes("google")){
-        
-        this.profilePicture = this.user.profile.picture;
-        console.log('picture',this.profilePicture)
-      }else{
-        console.log('no picture')
-      }
-    }*/
+    
   }
 }
